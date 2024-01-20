80a1d254f93bfe169aa227cafc264a5ffc872a6d
Michael <MichaelWidler@yahoo.com>
2024-01-20 15:44:19 -0800

    Put it all together.

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from simulation import *
 from game_record import *
+import statistics
 
 
 if __name__ == "__main__":
-   sim = Simulation()
-   sim.run()
-   print(sim.game_record)
+   end_turns = []
+   kills = []
+   for _ in range(1000000):
+      sim = Simulation()
+      sim.run()
+      end_turns.append(sim.game_record.ending_turn)
+      kills.append(sim.game_record.dead_hobbits)
+   print("+++ Results +++")
+   print("Duration:")
+   print("  Average: {:.2f}".format(statistics.mean(end_turns)))
+   print("  Median:  {}".format(int(statistics.median(end_turns))))
+   print("  Min:     {}".format(min(end_turns)))
+   print("  Max:     {}".format(max(end_turns)))
+   print("Dead Hobbits:")
+   print("  Average: {:.2f}".format(statistics.mean(kills)))
+   print("  Median:  {}".format(int(statistics.median(kills))))
+   print("  Min:     {}".format(min(kills)))
+   print("  Max:     {}".format(max(kills)))
