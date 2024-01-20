de47ac2170eab5b0cc8c20f424b7f1495150392a
Michael <MichaelWidler@yahoo.com>
2024-01-20 15:14:18 -0800

    Create simulation object.

---
+++ Simulation.py
@@ -0,0 +1,15 @@
+import RNG
+import Sauron
+import GameRecord
+
+class Simulation:
+
+   def __init__(self):
+      self.sauron = Sauron()
+      self.dead_hobbits = 0
+      self.turn_number = 0
+      self.game_record = GameRecord()
+   
+   def run(self):
+      pass
+   
