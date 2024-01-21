04c0e7a0cd7095797ef91044dde4f51fc5174fcf
Michael <MichaelWidler@yahoo.com>
2024-01-21 07:19:11 -0800

    Minor text changes.

--- main.py
+++ main.py
@@ -6,18 +6,20 @@ import statistics
 if __name__ == "__main__":
    end_turns = []
    kills = []
-   for _ in range(1000000):
+   for i in range(100000):
       sim = Simulation()
       sim.run()
       end_turns.append(sim.game_record.ending_turn)
       kills.append(sim.game_record.dead_hobbits)
+      if i % 1000 == 0:
+         print("{}k".format(i // 1000))
    print("+++ Results +++")
-   print("Duration:")
+   print("Duration (Turns):")
    print("  Average: {:.2f}".format(statistics.mean(end_turns)))
    print("  Median:  {}".format(int(statistics.median(end_turns))))
    print("  Min:     {}".format(min(end_turns)))
    print("  Max:     {}".format(max(end_turns)))
-   print("Dead Hobbits:")
+   print("Casualities (Dead Hobbits):")
    print("  Average: {:.2f}".format(statistics.mean(kills)))
    print("  Median:  {}".format(int(statistics.median(kills))))
    print("  Min:     {}".format(min(kills)))
